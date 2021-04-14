c53f0e375b35af4cfdd55b805bf5601f806b1bae
Dominic Padula <domopadula2192@gmail.com>
2021-04-13 18:33:31 -0600

    Registration Page Sad path and happy path for our users to register added routes for to create user resource for our database
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

--- spec/features/registration/registration_spec.rb
+++ spec/features/registration/registration_spec.rb
@@ -16,5 +16,64 @@ RSpec.describe 'Registration Page' do
       expect(page).to have_field('Password')
       expect(page).to have_field('Password Confirmation')
     end
+
+    it "can create users" do
+      visit registration_path
+
+      username = "funbucket13"
+      email = 'funbuck@gmail.com'
+      password = "test"
+      fill_in :username, with: username
+      fill_in :email, with: email
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: password
+      click_on 'Register'
+
+      expect(page).to have_content("Welcome, #{username}!")
+    end
+  end
+
+  describe 'sad path' do
+    it 'errors out with missing fields' do
+      visit registration_path
+
+      username = "funbucket13"
+      email = 'funbuck@gmail.com'
+      password = "test"
+      fill_in :username, with: username
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: password
+      click_on 'Register'
+      expect(current_path).to eq(registration_path)
+      expect(page).to have_content('You are missing fields. Both passwords must match.')
+    end
+
+    it 'errors out when we are missing user name' do
+      visit registration_path
+      username = "funbucket13"
+      email = 'funbuck@gmail.com'
+      password = "test"
+      fill_in :email, with: email
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: password
+      click_on 'Register'
+      expect(current_path).to eq(registration_path)
+      expect(page).to have_content('You are missing fields. Both passwords must match.')
+    end
+
+    it 'errors out when passwords are different' do
+      visit registration_path
+
+      username = "funbucket13"
+      email = 'funbuck@gmail.com'
+      password = "test"
+      fill_in :username, with: username
+      fill_in :email, with: email
+      fill_in :password, with: password
+      fill_in :password_confirmation, with: 'bad pass'
+      click_on 'Register'
+      expect(current_path).to eq(registration_path)
+      expect(page).to have_content('You are missing fields. Both passwords must match.')
+    end
   end
 end
