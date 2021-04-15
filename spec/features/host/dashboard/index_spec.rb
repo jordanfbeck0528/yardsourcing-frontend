bc02b57c9447c68ceb91d31f59f8945499c5bbf8
DougWelchons <doug.welchons@gmail.com>
2021-04-14 22:39:59 -0600

    Add tests and webmock stubs for creating new yard
    Co-authored-by: Genevieve Nuebel <72330302+Gvieve@users.noreply.github.com>

--- spec/features/host/dashboard/index_spec.rb
+++ spec/features/host/dashboard/index_spec.rb
@@ -23,6 +23,15 @@ describe 'As an authenticated user when I visit the host dashboard' do
   end
 
   it "I see a button to create a yard" do
+    stub_request(:get, "https://localhost:3001/api/v1/purposes").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v1.3.0'
+          }).
+        to_return(status: 200, body: '{"data":[{"id":"1","type":"purpose","attributes":{"name":"name1"}},{"id":"2","type":"purpose","attributes":{"name":"name2"}}]}', headers: {})
+        
     visit host_dashboard_index_path
 
     within '.header' do
@@ -38,6 +47,9 @@ describe 'As an authenticated user when I visit the host dashboard' do
   end
 
   xit "I see a section for all of my yards I have created" do
+    stub_request(:get, "localhost:3001/api/v1/yards").
+    to_return(status: 200, body: {"data":[{"id":"1","type":"yard","attributes":{"host_id":1,"name":"name","street_address":"address","city":"city","state":"state","zipcode":12345,"price":"20.25","description":"words and things","availability":"never","payment":"rubies only","photo_url_1":null,"photo_url_2":null,"photo_url_3":null}},{"id":"2","type":"yard","attributes":{"host_id":1,"name":"name2","street_address":"address2","city":"city2","state":"state2","zipcode":12342,"price":"20.00","description":"more words and things","availability":"never ever","payment":"rubies or gems  only","photo_url_1":null,"photo_url_2":null,"photo_url_3":null}}]}, headers: {})
+
     visit host_dashboard_index_path
 
     within '.my-yards' do
