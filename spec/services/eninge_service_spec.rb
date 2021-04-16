a40f8c1d3a2dabc6c92c4354c18f855b04ae5e06
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-15 22:12:45 -0400

    Add tests for engine service spec
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

---
+++ spec/services/eninge_service_spec.rb
@@ -0,0 +1,188 @@
+require 'rails_helper'
+
+RSpec.describe "EngineService", type: :feature do
+  describe "::all_purposes" do
+    it "should return json response of all purposes" do
+      response = File.open("spec/fixtures/all_purposes.json")
+      stub_request(:get, "https://localhost:3001/api/v1/purposes").
+         with(
+           headers: {
+       	  'Accept'=>'*/*',
+       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	  'User-Agent'=>'Faraday v1.3.0'
+          }).
+        to_return(status: 200, body: response, headers: {})
+
+      es = EngineService.all_purposes
+
+      expect(es[:data]).to be_an(Array)
+      expect(es[:data].first).to be_a(Hash)
+      expect(es[:data].first.keys).to eq([:id, :type, :attributes])
+      expect(es[:data].first[:type]).to eq("purpose")
+      expect(es[:data].first[:attributes].keys).to eq([:name])
+    end
+  end
+
+  describe "::create_yard(yard_params)" do
+    xit "Creates a yards and a json record is returned" do
+      response = File.open("spec/fixtures/yard_details.json")
+      yard_params = {:name=>"name",
+                     :description=>"description",
+                     :availability=>"availability",
+                     :payment=>"payment",
+                     :price=>"25.2",
+                     :street_address=>"street_address",
+                     :city=>"city",
+                     :state=>"state",
+                     :zipcode=>"zipcode",
+                     :photo_url_1=>"https://photo.com/path",
+                     :photo_url_2=>"",
+                     :photo_url_3=>"",
+                     :purposes=>["1", "3"]}
+     stub_request(:post, "https://localhost:3001/api/v1/yards").
+        with(
+          body: {"{\"yard\":{\"name\":\"name\",\"description\":\"description\",\"availability\":\"availability\",\"payment\":\"payment\",\"price\":\"25.2\",\"street_address\":\"street_address\",\"city\":\"city\",\"state\":\"state\",\"zipcode\":\"zipcode\",\"photo_url_1\":\"https://photo.com/path\",\"photo_url_2\":\"\",\"photo_url_3\":\"\",\"purposes\":"=>{"\"1\",\"3\"}}"=>nil}},
+          headers: {
+      	  'Accept'=>'*/*',
+      	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      	  'Content-Type'=>'application/x-www-form-urlencoded',
+      	  'User-Agent'=>'Faraday v1.3.0'
+          }).
+        to_return(status: 200, body: response, headers: {})
+      es = EngineService.create_yard(yard_params)
+      pry
+
+      expect(es[:data]).to be_a(Hash)
+      expect(es[:data].keys).to eq([:id, :type, :attributes])
+      expect(es[:data][:attributes].keys).to eq([ :host_id,
+                                                  :name,
+                                                  :street_address,
+                                                  :city,
+                                                  :state,
+                                                  :zipcode,
+                                                  :price,
+                                                  :description,
+                                                  :payment,
+                                                  :availability,
+                                                  :photo_url_1,
+                                                  :photo_url_2,
+                                                  :photo_url_3,
+                                                  :purposes])
+      expect(es[:data][:attributes][:purposes].keys).to eq([:data])
+      expect(es[:data][:attributes][:purposes][:data]).to be_an(Array)
+      expect(es[:data][:attributes][:purposes][:data].first).to be_a(Hash)
+      expect(es[:data][:attributes][:purposes][:data].first.keys).to eq([:id, :type, :attributes])
+      expect(es[:data][:attributes][:purposes][:data][:attributes].first.keys).to eq([:name])
+    end
+  end
+
+  describe "::yard_details" do
+    xit "should return json response of all purposes" do
+
+      es = EngineService.yard_details(1)
+      stub_request(:get, "https://localhost:3001/api/v1/hosts/#{1}/yards").
+         with(
+           headers: {
+       	  'Accept'=>'*/*',
+       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	  'User-Agent'=>'Faraday v1.3.0'
+           }).
+         to_return(status: 200, body: "", headers: {})
+
+
+
+
+      expect(es[:data]).to be_a(Hash)
+      expect(es[:data].keys).to eq([:id, :type, :attributes])
+      expect(es[:data][:attributes].keys).to eq([ :host_id,
+                                                  :name,
+                                                  :street_address,
+                                                  :city,
+                                                  :state,
+                                                  :zipcode,
+                                                  :price,
+                                                  :description,
+                                                  :payment,
+                                                  :availability,
+                                                  :photo_url_1,
+                                                  :photo_url_2,
+                                                  :photo_url_3,
+                                                  :purposes])
+      expect(es[:data][:attributes][:purposes].keys).to eq([:data])
+      expect(es[:data][:attributes][:purposes][:data]).to be_an(Array)
+      expect(es[:data][:attributes][:purposes][:data].first).to be_a(Hash)
+      expect(es[:data][:attributes][:purposes][:data].first.keys).to eq([:id, :type, :attributes])
+      expect(es[:data][:attributes][:purposes][:data][:attributes].first.keys).to eq([:name])
+    end
+  end
+
+  describe "::host_yards(host_id)" do
+    it "should return a list of a host's yards" do
+      # stub_omniauth_happy
+      # visit '/'
+      # click_button 'Login through Google'
+      es = EngineService.host_yards('123545')
+      response = File.open("spec/fixtures/host_yards.json")
+
+      stub_request(:get, "https://localhost:3001/api/v1/hosts/123545/yards").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v1.3.0'
+          }).
+        to_return(status: 200, body: "", headers: {})
+
+
+      many_yards_response_evaluation(es)
+    end
+  end
+
+  def many_yards_response_evaluation(es)
+    expect(es[:data]).to be_an(Array)
+    expect(es[:data].first).to be_a(Hash)
+    expect(es[:data].first.keys).to eq([:id, :type, :attributes])
+    expect(es[:data].first[:type]).to eq("yard")
+    expect(es[:data].first[:attributes].keys).to eq([ :host_id,
+                                                :name,
+                                                :street_address,
+                                                :city,
+                                                :state,
+                                                :zipcode,
+                                                :price,
+                                                :description,
+                                                :payment,
+                                                :availability,
+                                                :photo_url_1,
+                                                :photo_url_2,
+                                                :photo_url_3,
+                                                :purposes])
+    expect(es[:data].first[:attributes][:purposes][:data]).to be_an(Array)
+    expect(es[:data].first[:attributes][:purposes][:data].first.keys).to eq([:id, :type, :attributes])
+    expect(es[:data].first[:attributes][:purposes][:data].first[:attributes].keys).to eq([:name])
+  end
+
+  def yards_details_response_evaluation(es)
+    expect(es[:data]).to be_a(Hash)
+    expect(es[:data].keys).to eq([:id, :type, :attributes])
+    expect(es[:data][:attributes].keys).to eq([ :host_id,
+                                                :name,
+                                                :street_address,
+                                                :city,
+                                                :state,
+                                                :zipcode,
+                                                :price,
+                                                :description,
+                                                :payment,
+                                                :availability,
+                                                :photo_url_1,
+                                                :photo_url_2,
+                                                :photo_url_3,
+                                                :purposes])
+    expect(es[:data][:attributes][:purposes].keys).to eq([:data])
+    expect(es[:data][:attributes][:purposes][:data]).to be_an(Array)
+    expect(es[:data][:attributes][:purposes][:data].first).to be_a(Hash)
+    expect(es[:data][:attributes][:purposes][:data].first.keys).to eq([:id, :type, :attributes])
+    expect(es[:data][:attributes][:purposes][:data][:attributes].first.keys).to eq([:name])
+  end
+end
