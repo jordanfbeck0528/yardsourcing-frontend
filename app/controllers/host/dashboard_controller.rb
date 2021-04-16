ebebfdd0408b02c87fb7baa234e3a180f8f2f0a7
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-16 14:52:30 -0400

    Complete host yards method happy path

--- app/controllers/host/dashboard_controller.rb
+++ app/controllers/host/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Host::DashboardController < ApplicationController
   def index
-    info = EngineService.host_yards(current_user.id)
+    @host_yards = EngineService.host_yards(current_user.id)[:data]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   def create
+    # @user = User.find_or_create_from_auth_hash(auth_hash)
     user = User.from_omniauth(request.env['omniauth.auth'])
+
     session[:user_id] = user.id
     flash[:success] = "Welcome, #{user.username}"
     redirect_to host_dashboard_index_path
@@ -9,5 +11,11 @@ class SessionsController < ApplicationController
   def bad
     flash[:error] = "Sorry, your credentials are bad."
     redirect_to root_path
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
