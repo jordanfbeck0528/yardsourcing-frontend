db44cb43deaaaed18be86f143b7852fe4b72b8a9
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-16 14:51:45 -0400

    Remove validation has_secure_password, not needed due to omni-auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   before_save {email.try(:downcase)}
-  has_secure_password
 
   def self.from_omniauth(response)
     User.find_or_create_by(uid: response[:uid]) do |u|
