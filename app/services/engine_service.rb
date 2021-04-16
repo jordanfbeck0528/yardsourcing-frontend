aabb426492b9dc0edec153efee4c399f3ef738bb
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-16 16:27:29 -0400

    Final tweaks from pr comments

--- app/services/engine_service.rb
+++ app/services/engine_service.rb
@@ -24,7 +24,6 @@ class EngineService
     url = "/api/v1/hosts/#{host_id}/yards"
     response = connection.get(url)
     data = response.body
-    binding.pry
     JSON.parse(data, symbolize_names: true)
   end
 
