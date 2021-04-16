80d2e83319437fa3124ccddd2df5c237d663f1db
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-16 07:47:25 -0400

    Add describe block to new tests related to elements on the view page
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

--- spec/features/host/yards/new_spec.rb
+++ spec/features/host/yards/new_spec.rb
@@ -12,56 +12,57 @@ describe 'As an authenticated user when I visit the new yard page' do
         to_return(status: 200, body: '{"data":[{"id":"1","type":"purpose","attributes":{"name":"name1"}},{"id":"2","type":"purpose","attributes":{"name":"name2"}},{"id":"3","type":"purpose","attributes":{"name":"name3"}}]}', headers: {})
 
         stub_request(:post, "https://localhost:3001/api/v1/yards").
-                 with(
-                   body: {"{\"yard\":{\"name\":\"name\",\"description\":\"description\",\"availability\":\"availability\",\"payment\":\"payment\",\"price\":\"25.2\",\"street_address\":\"street_address\",\"city\":\"city\",\"state\":\"state\",\"zipcode\":\"zipcode\",\"photo_url_1\":\"https://photo.com/path\",\"photo_url_2\":\"\",\"photo_url_3\":\"\",\"purposes\":"=>{"\"1\",\"3\"}}"=>nil}},
-                   headers: {
-               	  'Accept'=>'*/*',
-               	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-               	  'Content-Type'=>'application/x-www-form-urlencoded',
-               	  'User-Agent'=>'Faraday v1.3.0'
-                   }).
-                 to_return(status: 200, body: "", headers: {})
+           with(
+             body: {"{\"yard\":\"{\\\"host_id\\\""=>">#{user.id}, \\\"name\\\"=>\\\"name\\\", \\\"description\\\"=>\\\"description\\\", \\\"availability\\\"=>\\\"availability\\\", \\\"payment\\\"=>\\\"payment\\\", \\\"price\\\"=>\\\"25.2\\\", \\\"street_address\\\"=>\\\"street_address\\\", \\\"city\\\"=>\\\"city\\\", \\\"state\\\"=>\\\"state\\\", \\\"zipcode\\\"=>\\\"zipcode\\\", \\\"photo_url_1\\\"=>\\\"https://photo.com/path\\\", \\\"photo_url_2\\\"=>\\\"\\\", \\\"photo_url_3\\\"=>\\\"\\\", \\\"purposes\\\"=>[\\\"1\\\", \\\"3\\\"]}\"}"},
+             headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type'=>'application/x-www-form-urlencoded',
+            'User-Agent'=>'Faraday v1.3.0'
+             }).
 
       stub_omniauth_happy
       visit root_path
       click_button 'Login through Google'
   end
 
-  it "I see a form with name, description, availability, payment, and price" do
-    visit new_host_yard_path
+  describe  "there are elements on the dashboard" do
+    it "I see a form with name, description, availability, payment, and price" do
+      visit new_host_yard_path
 
-    expect(page).to have_field('name')
-    expect(page).to have_field('description')
-    expect(page).to have_field('availability')
-    expect(page).to have_field('payment')
-    expect(page).to have_field('price')
-  end
+      expect(page).to have_field('name')
+      expect(page).to have_field('description')
+      expect(page).to have_field('availability')
+      expect(page).to have_field('payment')
+      expect(page).to have_field('price')
+    end
 
-  it "I see a section with address, city, state, zipcode" do
-    visit new_host_yard_path
+    it "I see a section with address, city, state, zipcode" do
+      visit new_host_yard_path
 
-    expect(page).to have_field('street_address')
-    expect(page).to have_field('city')
-    expect(page).to have_field('state')
-    expect(page).to have_field('zipcode')
-  end
+      expect(page).to have_field('street_address')
+      expect(page).to have_field('city')
+      expect(page).to have_field('state')
+      expect(page).to have_field('zipcode')
+    end
 
-  it "I see a section to add up to three photo urls" do
-    visit new_host_yard_path
+    it "I see a section to add up to three photo urls" do
+      visit new_host_yard_path
 
-    expect(page).to have_field('photo_url_1')
-    expect(page).to have_field('photo_url_2')
-    expect(page).to have_field('photo_url_3')
-  end
+      expect(page).to have_field('photo_url_1')
+      expect(page).to have_field('photo_url_2')
+      expect(page).to have_field('photo_url_3')
+    end
 
-  it "I see a section with checkboxes for allowable uses and button to submit" do
-    visit new_host_yard_path
+    it "I see a section with checkboxes for allowable uses and button to submit" do
+      visit new_host_yard_path
 
-    expect(page).to have_content('Select all purposes you will allow others to rent your yard for')
-    expect(page).to have_unchecked_field('purposes_1')
-    expect(page).to have_unchecked_field('purposes_2')
-    expect(page).to have_unchecked_field('purposes_3')
-    expect(page).to have_button('Create Yard')
+      expect(page).to have_content('Select all purposes you will allow others to rent your yard for')
+      expect(page).to have_unchecked_field('purposes_1')
+      expect(page).to have_unchecked_field('purposes_2')
+      expect(page).to have_unchecked_field('purposes_3')
+      expect(page).to have_button('Create Yard')
+    end
   end
 
   describe 'happy path' do
