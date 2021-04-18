4e556488ecca60ae371ac381931b296901b1c8eb
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-18 14:37:29 -0400

    Add test for yard show page. Yard show page will not be nested by host or renter

---
+++ spec/features/yards/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Yard Show Page" do
+  describe "as an authenticated user" do
+    it "displays the yard's information" do
+      
+
+    end
+
+    it "displays a button to 'Edit' the yard if the current user is the host" do
+
+    end
+    it "displays a button to 'Rent' the yard if the current user is the renter" do
+
+    end
+  end
+end
