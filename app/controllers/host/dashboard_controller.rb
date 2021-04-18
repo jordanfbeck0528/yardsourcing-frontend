f84803837b826eac70bc4218f2a779ba073fc642
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-18 12:53:07 -0400

    Remove commented out code

--- app/controllers/host/dashboard_controller.rb
+++ app/controllers/host/dashboard_controller.rb
@@ -1,24 +1,5 @@
 class Host::DashboardController < ApplicationController
   def index
     @host_yards = DashboardFacade.host_yards(current_user.id)
-    # host_yards = EngineService.host_yards(current_user.id)
-    # @host_yards = host_yards[:data].map do |yard|
-    #    OpenStruct.new({ name:         yard[:attributes][:name],
-    #                     id:           yard[:id],
-    #                     description:  yard[:attributes][:description],
-    #                     address:      full_address(yard),
-    #                     price:        yard[:attributes][:price],
-    #                     purposes:        all_purposes(yard) })
-    #   end
   end
-
-  # def full_address(yard)
-  #   "#{yard[:attributes][:street_address]}, #{yard[:attributes][:city]}, #{yard[:attributes][:state]}, #{yard[:attributes][:zipcode]}"
-  # end
-  #
-  # def all_purposes(yard)
-  #   yard[:attributes][:purposes][:data].map do |purpose|
-  #     purpose[:attributes][:name]
-  #   end
-  # end
 end
