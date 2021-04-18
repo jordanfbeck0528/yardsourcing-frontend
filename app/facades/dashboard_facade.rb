87225e7a71d721b345123c97429949a4118aed4b
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-18 12:51:50 -0400

    Refactor host_yards method to utilize a facade

---
+++ app/facades/dashboard_facade.rb
@@ -0,0 +1,24 @@
+class DashboardFacade
+
+  def self.host_yards(host_id)
+    host_yards = EngineService.host_yards(host_id)
+    host_yards = host_yards[:data].map do |yard|
+       OpenStruct.new({ name:         yard[:attributes][:name],
+                        id:           yard[:id],
+                        description:  yard[:attributes][:description],
+                        address:      full_address(yard),
+                        price:        yard[:attributes][:price],
+                        purposes:        all_purposes(yard) })
+      end
+  end
+
+  def self.full_address(yard)
+    "#{yard[:attributes][:street_address]}, #{yard[:attributes][:city]}, #{yard[:attributes][:state]}, #{yard[:attributes][:zipcode]}"
+  end
+
+  def self.all_purposes(yard)
+    yard[:attributes][:purposes][:data].map do |purpose|
+      purpose[:attributes][:name]
+    end
+  end
+end
