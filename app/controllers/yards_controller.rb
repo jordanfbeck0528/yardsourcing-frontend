d5d4d2d40cca3961481698b26dde53a114fed0d3
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-18 16:46:45 -0400

    Refactor yards_controller and show method to use YardsFacade

--- app/controllers/yards_controller.rb
+++ app/controllers/yards_controller.rb
@@ -1,41 +1,7 @@
 class YardsController < ApplicationController
 
   def show
-    yard = EngineService.yard_details(params[:id])
-    if yard == {}
-      @yard = {}
-    elsif
-      @yard = OpenStruct.new({  name:        yard[:attributes][:name],
-                          host_id:     yard[:attributes][:host_id],
-                           id:           yard[:id],
-                           description:  yard[:attributes][:description],
-                           address:      full_address(yard),
-                           price:        yard[:attributes][:price],
-                           purposes:     all_purposes(yard) })
-
-      if current_user.id == @yard.host_id
-        @button_params = {}
-        @button_params[:text] = "Edit"
-        @button_params[:path] = host_yards_path(current_user.id)
-        @button_params[:method] = :patch
-      else
-        @button_params = {}
-        @button_params[:text] = "Rent"
-        @button_params[:path] = "bookings/new"
-        @button_params[:method] = :post
-      end
-    end
-  end
-
-  private
-
-  def full_address(yard)
-    "#{yard[:attributes][:street_address]}, #{yard[:attributes][:city]}, #{yard[:attributes][:state]}, #{yard[:attributes][:zipcode]}"
-  end
-
-  def all_purposes(yard)
-    yard[:attributes][:purposes][:data].map do |purpose|
-      purpose[:attributes][:name]
-    end
+    @yard = YardsFacade.yard_details(params[:id])
+    @button_params = YardsFacade.button_params(current_user.id)
   end
 end
