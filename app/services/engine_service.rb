5fcdfab61d4ff8b51f8fdd186558a63f00ec69e7
Jenny Branham <branhamjenny@gmail.com>
2021-04-19 10:23:22 -0500

    Small changes suggested by Ian
    Co-authored-by: Alexa Smyth <73140135+amsmyth1@users.noreply.github.com>
    Co-authored-by: Angel Breaux <48256547+abreaux26@users.noreply.github.com>
    Co-authored-by: Dominic Padula <70593322+domo2192@users.noreply.github.com>
    Co-authored-by: Doug Welchons <72056427+DougWelchons@users.noreply.github.com>
    Co-authored-by: Genevieve Nuebel <72330302+Gvieve@users.noreply.github.com>
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

--- app/services/engine_service.rb
+++ app/services/engine_service.rb
@@ -2,31 +2,26 @@ class EngineService
 
   def self.all_purposes
     response = connection.get('/api/v1/purposes')
-    data = response.body
-    JSON.parse(data, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.create_yard(yard_params)
-
     response = connection.post('/api/v1/yards') do |req|
       req.headers["CONTENT_TYPE"] = "application/json"
       req.params = yard_params
     end
-    
-    data = response.body
-    JSON.parse(data, symbolize_names: true)[:data]
+
+    JSON.parse(response.body, symbolize_names: true)[:data]
   end
 
   def self.yard_details(yard_id)
     response = connection.get("/api/v1/yards/#{yard_id}")
-    data = response.body
-    JSON.parse(data, symbolize_names: true)[:data]
+    JSON.parse(response.body, symbolize_names: true)[:data]
   end
 
   def self.host_yards(host_id)
     response = connection.get("/api/v1/hosts/#{host_id}/yards")
-    data = response.body
-    info = JSON.parse(data, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.connection
