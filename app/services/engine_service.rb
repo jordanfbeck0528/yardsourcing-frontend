39416c1572853e503f9bf233a839a5ccd1132fc1
Dominic Padula <domopadula2192@gmail.com>
2021-04-19 16:57:06 -0600

    Sad Path Testing refactor engine service to pass data, refactor controller for before action to grab purposes for sad paths, write sad paths tests for any piece of form that is missing
    Co-authored-by: Doug Welchons <72056427+DougWelchons@users.noreply.github.com>

--- app/services/engine_service.rb
+++ app/services/engine_service.rb
@@ -1,29 +1,23 @@
 class EngineService
-
   def self.all_purposes
     response = connection.get('/api/v1/purposes')
     JSON.parse(response.body, symbolize_names: true)
   end
-
   def self.create_yard(yard_params)
     response = connection.post('/api/v1/yards') do |req|
       req.headers["CONTENT_TYPE"] = "application/json"
       req.params = yard_params
     end
-
-    JSON.parse(response.body, symbolize_names: true)[:data]
+    JSON.parse(response.body, symbolize_names: true)
   end
-
   def self.yard_details(yard_id)
     response = connection.get("/api/v1/yards/#{yard_id}")
     JSON.parse(response.body, symbolize_names: true)[:data]
   end
-
   def self.host_yards(host_id)
     response = connection.get("/api/v1/hosts/#{host_id}/yards")
     JSON.parse(response.body, symbolize_names: true)
   end
-
   def self.connection
     Faraday.new(url: ENV['ys_engine_url'])
   end
