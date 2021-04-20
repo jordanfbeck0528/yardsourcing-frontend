fc0e167a4f5460336b3e65d403ab165cfa0c4fd1
abreaux26 <angelbreaux26@gmail.com>
2021-04-20 12:54:43 -0500

    Add renter yard pending facade call
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

--- app/controllers/renter/dashboard_controller.rb
+++ app/controllers/renter/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Renter::DashboardController < ApplicationController
   def index
     @renter_yards_approved = DashboardFacade.renter_yards(current_user.id, 'approved')
+    @renter_yards_pending = DashboardFacade.renter_yards(current_user.id, 'pending')
   end
 end
