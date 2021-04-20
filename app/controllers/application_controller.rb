cd3660c8ea9482dc318ef553c41b2e7b273afa81
Dominic Padula <domopadula2192@gmail.com>
2021-04-20 06:45:01 -0600

    memoize memoizise? memomization????

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
   end
 
   def set_purposes
-    info = EngineService.all_purposes
-    @purposes = info[:data].map do |obj_info|
+    @info ||= EngineService.all_purposes
+    @purposes = @info[:data].map do |obj_info|
       OpenStruct.new({ id: obj_info[:id],
                        name: obj_info[:attributes][:name].titleize})
-    end 
+    end
   end
 end
