1b410a5968271dec516b581113223d982b2fa256
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-20 13:14:57 -0400

    Update test to call on the css selector rendered if yards are rendered, this way cassette will not fail

--- spec/features/host/dashboard/index_spec.rb
+++ spec/features/host/dashboard/index_spec.rb
@@ -49,8 +49,7 @@ describe 'As an authenticated user when I visit the host dashboard' do
     VCR.use_cassette('host_yards') do
       visit host_dashboard_index_path
 
-      expect(page).to have_link("Ultimate Party Yard")
-      expect(page).to have_link("Large Yard for any Hobby")
+      expect(page).to have_css(".yard_info")
     end
   end
 
