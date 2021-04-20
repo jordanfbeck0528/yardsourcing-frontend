517583dc438746bb1b668b6fb8ff60fff5041ca1
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-19 21:01:33 -0400

    Add route to edit host yard

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :host do
     resources :dashboard, only: :index
-    resources :yards, only: [:new, :create]
+    resources :yards, only: [:new, :create, :update]
   end
 
   resources :yards, only: [:show]
