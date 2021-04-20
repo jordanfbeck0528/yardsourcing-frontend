c4278312fbf5dffdfff98b5b86697ec2538cfff0
abreaux26 <angelbreaux26@gmail.com>
2021-04-20 11:21:28 -0500

    Add renter yard method to facade
    Co-authored-by: Jordan Beck <68141454+jordanfbeck0528@users.noreply.github.com>

--- app/facades/dashboard_facade.rb
+++ app/facades/dashboard_facade.rb
@@ -11,6 +11,23 @@ class DashboardFacade
       end
   end
 
+  def self.renter_yards(renter_id, status)
+    renter_yards = EngineService.renter_yards(renter_id, status)
+    renter_yards = renter_yards[:data].map do |yard|
+      yard_info = yard_info(yard[:attributes][:yard_id])
+      OpenStruct.new({ name:       yard[:attributes][:booking_name],
+                       address:    full_address(yard_info),
+                       date:       yard[:attributes][:date],
+                       duration:   yard[:attributes][:duration],
+                       total_cost: total_cost(yard[:attributes][:duration], yard_info[:attributes][:price]),
+                       img:        yard_info[:attributes][:photo_url_1] })
+    end
+  end
+
+  def self.yard_info(yard_id)
+    EngineService.yard_details(yard_id)
+  end
+
   def self.full_address(yard)
     "#{yard[:attributes][:street_address]} #{yard[:attributes][:city]}, #{yard[:attributes][:state]} #{yard[:attributes][:zipcode]}"
   end
@@ -19,5 +36,9 @@ class DashboardFacade
     yard[:attributes][:purposes][:data].map do |purpose|
       purpose[:attributes][:name]
     end
+  end
+
+  def self.total_cost(duration, price)
+    (duration * price).round(2)
   end
 end
