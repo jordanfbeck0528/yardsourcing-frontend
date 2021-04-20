cccd58cb394ec88e4adc18c53838696eff40aaa5
abreaux26 <angelbreaux26@gmail.com>
2021-04-20 11:23:28 -0500

    Remove pry

--- app/controllers/renter/dashboard_controller.rb
+++ app/controllers/renter/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class Renter::DashboardController < ApplicationController
   def index
     @renter_yards_approved = DashboardFacade.renter_yards(current_user.id, 'approved')
-    binding.pry
   end
 end
