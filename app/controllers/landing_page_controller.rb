17c75f7c39795b495e4e42c46db4df769c9d7ee8
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-04-21 10:50:18 -0400

    Make array of dummy photos privat method

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,7 +1,12 @@
 class LandingPageController < ApplicationController
   def index
-    # require "pry"; binding.pry
-    all_photos = [ "https://cdn.pixabay.com/photo/2015/03/26/10/08/pool-691008_960_720.jpg",
+    @display_photos = all_photos.sample(3)
+  end
+
+  private
+
+  def all_photos
+    [ "https://cdn.pixabay.com/photo/2015/03/26/10/08/pool-691008_960_720.jpg",
       "https://cdn.pixabay.com/photo/2016/11/25/19/02/soccer-1859046_960_720.jpg",
       "https://cdn.pixabay.com/photo/2015/09/09/18/40/dog-932447_960_720.jpg",
       "https://cdn.pixabay.com/photo/2016/07/28/06/05/white-1547092_960_720.jpg",
@@ -9,7 +14,5 @@ class LandingPageController < ApplicationController
       "https://cdn.pixabay.com/photo/2014/04/10/15/39/pavilion-321036_960_720.jpg",
       "https://cdn.pixabay.com/photo/2015/03/26/10/08/pool-691008_960_720.jpg",
       "https://cdn.pixabay.com/photo/2015/03/26/10/29/swings-691446_960_720.jpg"].uniq
-
-    @display_photos = all_photos.sample(3)
   end
 end
