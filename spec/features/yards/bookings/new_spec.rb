56d9b90acfc891057ec4bf590e9751ecad904f10
Gvieve <gnuebel13@gmail.com>
2021-04-21 14:27:48 -0600

    Update yard booking new_spec and re-record cassettes

--- spec/features/yards/bookings/new_spec.rb
+++ spec/features/yards/bookings/new_spec.rb
@@ -9,22 +9,20 @@ RSpec.describe "New Booking Page" do
     end
 
     it "I see a form with the yard name, event name, date, start time, duration, and description" do
-      VCR.use_cassette('host_yard_show_page_ultimate_party') do
-        VCR.use_cassette('booking/new_booking') do
-          visit new_yard_booking_path(2)
-          expect(page).to have_content("Yard: Ultimate Party Yard")
-          expect(page).to have_field('booking_name')
-          expect(page).to have_field('date')
-          expect(page).to have_field('time')
-          expect(page).to have_field('duration')
-          expect(page).to have_field('description')
-        end
+      VCR.use_cassette('yards/bookings/new_booking_yard_2') do
+        visit new_yard_booking_path(2)
+        expect(page).to have_content("Yard: Ultimate Party Yard")
+        expect(page).to have_field('booking_name')
+        expect(page).to have_field('date')
+        expect(page).to have_field('time')
+        expect(page).to have_field('duration')
+        expect(page).to have_field('description')
       end
     end
 
     describe 'happy path' do
       it "when I fill out the form with valid information I can create a booking" do
-        VCR.use_cassette('booking/create_booking') do
+        VCR.use_cassette('yards/bookings/new_booking_yard_2_submit') do
           visit new_yard_booking_path(2)
           fill_in :booking_name, with: "A new booking!!"
           fill_in :date, with: Date.new(2021, 05,05)
@@ -41,7 +39,7 @@ RSpec.describe "New Booking Page" do
 
     describe 'sad path' do
       it "when I do not enter a name I cannot create a booking" do
-        VCR.use_cassette('booking/bad-booking') do
+        VCR.use_cassette('yards/bookings/new_booking_yard_2_no_name') do
           visit new_yard_booking_path(2)
           fill_in :date, with: Date.new(2021, 05,05)
           fill_in :time, with: Time.new(2021,05,05,12)
@@ -52,7 +50,7 @@ RSpec.describe "New Booking Page" do
         end
       end
       it "when I do not enter a date I cannot create a booking" do
-        VCR.use_cassette('booking/bad-booking-date') do
+        VCR.use_cassette('yards/bookings/new_booking_yard_2_no_date') do
           visit new_yard_booking_path(2)
           fill_in :booking_name, with: "A new booking!!"
           fill_in :time, with: Time.new(2021,05,05,12)
@@ -64,7 +62,7 @@ RSpec.describe "New Booking Page" do
       end
 
       it 'when I do not enter a time I cannot create a booking' do
-        VCR.use_cassette('booking/bad-booking-time') do
+        VCR.use_cassette('yards/bookings/new_booking_yard_2_no_time') do
           visit new_yard_booking_path(2)
           fill_in :booking_name, with: "A new booking!!"
           fill_in :date, with: Date.new(2021, 05,05)
@@ -76,7 +74,7 @@ RSpec.describe "New Booking Page" do
       end
 
       it 'when I do not enter a duration I cannot create a booking' do
-        VCR.use_cassette('booking/bad-booking-duration') do
+        VCR.use_cassette('yards/bookings/new_booking_yard_2_no_duration') do
           visit new_yard_booking_path(2)
           fill_in :booking_name, with: "A new booking!!"
           fill_in :date, with: Date.new(2021, 05,05)
@@ -88,7 +86,7 @@ RSpec.describe "New Booking Page" do
       end
 
       it 'when I do not enter a description I cannot create a booking' do
-        VCR.use_cassette('booking/bad-booking-description') do
+        VCR.use_cassette('yards/bookings/new_booking_yard_2_no_description') do
           visit new_yard_booking_path(2)
           fill_in :booking_name, with: "A new booking!!"
           fill_in :date, with: Date.new(2021, 05,05)
@@ -100,7 +98,7 @@ RSpec.describe "New Booking Page" do
       end
 
       it 'when I enter a date from the past I cannot create a booking' do
-        VCR.use_cassette('booking/bad-booking-date-from-the-past') do
+        VCR.use_cassette('yards/bookings/new_booking_yard_2_date_in_past') do
           visit new_yard_booking_path(2)
           fill_in :booking_name, with: "A new booking!!"
           fill_in :date, with: Date.new(2021, 03,05)
