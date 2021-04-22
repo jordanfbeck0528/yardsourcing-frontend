fa1d21a92be92d3dfc24d67213ad3dd51235e95e
abreaux26 <angelbreaux26@gmail.com>
2021-04-22 00:41:19 -0500

    Clean up code and get testing at 100%

--- spec/features/landing_page/landing_spec.rb
+++ spec/features/landing_page/landing_spec.rb
@@ -35,5 +35,21 @@ RSpec.describe 'Welcome Page' do
         expect(current_path).to eq(renter_dashboard_index_path)
       end
     end
+
+    it 'logs user out' do
+      user = User.create!(id:1, uid: '123545', username: 'Dominic Padula', email:'thisemail@gmail.com')
+      omniauth_response = stub_omniauth_happy('123545', 'Dominic Padula', 'thisemail@gmail.com')
+      user_1 = User.from_omniauth(omniauth_response)
+      response = File.open("spec/fixtures/host_yards.json")
+
+      VCR.use_cassette('renters/dash/landing_page') do
+        visit root_path
+
+        click_button 'Login through Google'
+        click_button 'Logout'
+        expect(page).to have_content("You have been logged out.")
+        expect(current_path).to eq(root_path)
+      end
+    end
   end
 end
